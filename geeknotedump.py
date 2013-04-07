32b4fd3b6d19dcd3a8d65688443067fc90cebc08
xredor <xredor@xredor-laptop.(none)>
2013-04-07 13:58:06 +0400

    evernote dumper added

---
+++ geeknotedump.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+#./geeknote.py login
+from os import system, popen
+
+
+def preadlines(cmd, split=True):
+    f = popen(cmd,"r")
+    r = f.read()
+    if split:
+        r = r.splitlines()
+    f.close()
+    return r
+
+system('mkdir -p data')
+
+notebookslist = preadlines("./geeknote.py notebook-list | awk -F\" : \" '{print $2}' | grep .")
+
+for nb in notebookslist:
+    print nb
+    system("mkdir -p data/%s" % nb)
+    notes = preadlines("./geeknote.py find --notebooks %s | grep ." % nb)
+    for n in notes:
+        try:
+            n = n.split(' : ')
+            id = n[0]
+            name = n[1]
+            name = name[12:].replace(' ','_')+'-'+name[0:12]
+            print id, name
+            system("./geeknote.py show --note %s > \"data/%s/%s\"" % (id, nb, name))
+        except:
+            print 'Error while getting', n
